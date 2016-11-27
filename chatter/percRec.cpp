5e8790704966e10ed1ae6a3fe7375457cfc63cc8
Vishaal Munusamy Kabilan <mkvishaal@gmail.com>
2016-11-27 18:52:12 -0500

    Create percRec.cpp

---
+++ chatter/percRec.cpp
@@ -0,0 +1,20 @@
+#include "ros/ros.h"
+#include "geomtry_msgs/PoseArray.h"
+#include "geometry_msgs/Pose.h"
+
+void percCallback(const geometry_msgs::PoseArray& points){
+	int size = points.poses.size();
+	for(int i=0;i<=size;i++){
+		geometry_msgs::PoseArray targetPose;
+		targetPose = points.poses[i];
+		waypoints.push_back(targetPose);
+        }
+}
+int main(int argc, char **argv){
+	ros::init(argc,argv,"percRec");
+	ros::NodeHandle n;
+	ros::Subscriber sub - n.subscribe("percTopic",100, percCallback);
+	ros::spin();
+	return 0;
+}
+
